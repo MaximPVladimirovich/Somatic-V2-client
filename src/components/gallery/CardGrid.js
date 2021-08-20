72ed134b2d34c210b7b4a5490449729b3f5c6db8
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-20 17:23:21 -0500

    Mapping through studios successfull

--- src/components/gallery/CardGrid.js
+++ src/components/gallery/CardGrid.js
@@ -1,51 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import StudioCard from './StudioCard'
+// import StudioCard from './StudioCard'
 // import Card from '@material-ui/core/Card';
 // import CardActions from '@material-ui/core/CardActions';
 // import CardContent from '@material-ui/core/CardContent';
 // import Typography from '@material-ui/core/Typography';
 
-// const useStyles = makeStyles({
+const useStyles = makeStyles({
 
-// });
+});
 
 export default function CardGrid() {
-  const [studios, setStudios] = useState([])
+  const [studios, setStudios] = useState(null)
 
-  const getStudioData = async () => {
-    await fetch("http://127.0.0.1:3000/")
-      .then(response => response.json())
-      .then(json => {
-        setStudios([json])
-      });
+
+  const getStudioData = async function () {
+    try {
+      const req = await fetch("http://127.0.0.1:3001")
+      const data = await req.json()
+      console.log(data)
+      setStudios(data.studios)
+    } catch (error) {
+      console.log(error)
+    }
+
   };
 
   useEffect(() => {
     getStudioData()
+
   }, [])
 
-  // const classes = useStyles();
+  const classes = useStyles();
 
-  let loaded = function () {
-    return (
-      <div>{
-        studios.forEach(function (studio) {
-          return <StudioCard studio={studio} />
-        })
-      }
-        <p>
-          {JSON.stringify(studios, null)}
-        </p>
-      </div>
-    )
-
+  const loaded = function () {
+    return studios.map(studio => {
+      return (
+        <div>
+          <h1>{studio.name}</h1>
+        </div >);
+    })
   }
-
   return (
-    <div className="projects">
-        {studios ? loaded() : <h1>Loading...</h1>}
+    <div>
+      {studios ? loaded() : <h1>Loading...</h1>}
     </div>
-)
+  )
 }
+
+
+
 
---
+++ src/components/gallery/StudioCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function StudioCard(props) {
+
+
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+
+  )
+}
