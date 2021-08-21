8f44b87939ce5b38a8a7eb38e5f158bb56d19ed5
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-20 21:58:07 -0500

    added image

--- src/components/home_image/Home_image.js
+++ src/components/home_image/Home_image.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardActionArea from '@material-ui/core/CardActionArea'
 
 const useStyles = makeStyles((theme) => ({
-  home_banner: {
-    margin: 'auto'
-  }
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    maxHeight: 400,
+  },
 }))
 
 
 export default function HomeImage() {
   const classes = useStyles()
   return (
-    <Grid container >
-      <Grid item xs={12} className={classes.home_banner}>
-        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Ffreefuninaustin.com%2Fwp-content%2Fuploads%2Fsites%2F44%2F2015%2F08%2FScreen-Shot-2015-08-13-at-8.37.09-AM.png%3Fssl%3D1&f=1&nofb=1' alt='dance class'></img>
-      </Grid>
-    </Grid>
+    <Card >
+      <CardActionArea>
+        <CardMedia className={classes.media} image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Ffreefuninaustin.com%2Fwp-content%2Fuploads%2Fsites%2F44%2F2015%2F08%2FScreen-Shot-2015-08-13-at-8.37.09-AM.png%3Fssl%3D1&f=1&nofb=1' title='dance class' />
+      </CardActionArea>
+    </Card>
+
   )
 }
